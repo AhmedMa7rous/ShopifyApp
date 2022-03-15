55d74ec08d208cf6b7d19897c09891cd027facf0
ahmedmosthalim <ahmed_mostafa_halim@yahoo.com>
2022-03-16 01:46:43 +0200

    static data for Product details

---
+++ ShopifyApp/View/Controller/BrandProductsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BrandProductsViewController.swift
+//  ShopifyApp
+//
+//  Created by Ahmed Mostafa on 16/03/2022.
+//  Copyright © 2022 Ma7rous. All rights reserved.
+//
+
+import UIKit
+
+class BrandProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
