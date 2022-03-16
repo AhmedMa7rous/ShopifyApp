f2267ce9e0837a302a742472578dfdd1cc31b94f
AbdallahEhab00 <55344010+AbdallahEhab00@users.noreply.github.com>
2022-03-16 17:25:23 +0200

    home screen with Brands and Sales Commit

--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 platform :ios, '9.0'
 
 target 'ShopifyApp' do
+  use_frameworks!
+
 pod 'SDWebImage', '~> 5.0'
 end
--- ShopifyApp/Model/Network Services/NetworkService.swift
+++ ShopifyApp/Model/Network Services/NetworkService.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+class NetworkService {
+    
+    
+    enum ApiError : Error {
+        case fetchBrandFail
+    }
+    
+    // Make request for all  Api URl
+    private func createRequest(url:URL?,completion:(URLRequest)->Void){
+        
+        guard let url = url else {
+            return
+        }
+        let request = URLRequest(url: url)
+        completion(request)
+    }
+    
+    public func getAllBrands(completion: @escaping(Result<[Smart_collections]?,Error>)->Void){
+        createRequest(url:URL(string:UrlConstants.categoryUrl)) { request in
+            
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                
+                guard let data = data , error == nil else {
+                    completion(.failure(ApiError.fetchBrandFail))
+                    return
+                }
+                do{
+//                    let result = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+//                    print(result)
+                    let result = try JSONDecoder().decode(SmartCollectionsKey.self, from: data)
+                    completion(.success(result.smart_collections))
+                    
+                }catch{
+                    completion(.failure(ApiError.fetchBrandFail))
+                }
+            }
+            
+            task.resume()
+        }
+        
+    }
+    
+    
+    
+}
