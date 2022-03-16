32d7ea45adc2844929d299403d6f4e321bd7b018
ahmedmosthalim <ahmed_mostafa_halim@yahoo.com>
2022-03-16 20:08:02 +0200

    Revert "Merge branch 'halim' into Abdallah"
    This reverts commit a48547f4d579696acb4a2230163f6f6263d6d833, reversing
    changes made to f2267ce9e0837a302a742472578dfdd1cc31b94f.

--- ShopifyApp/View/Controller/CategoryViewController.swift
+++ ShopifyApp/View/Controller/CategoryViewController.swift
@@ -9,40 +9,12 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-    /*============================================*/
-    
-    //MARK: proparty
-    lazy var viewModel: CategoryViewControllerModel = {
-           return CategoryViewControllerModel()
-       }()
-    /*============================================*/
-        
-    //MARK: Outlet Connections
-    
-    @IBOutlet var segmentedControl: UISegmentedControl!
-    @IBOutlet var productsClollectionView: UICollectionView!
+
     /*============================================*/
     
     //MARK: Outlet Actions
     
-    @IBAction func changeCatagory(_ sender: Any) {
-        switch segmentedControl.selectedSegmentIndex
-        {
-        case 0:
-            self.fetchProductsWithIB(collection_id: "272069099567")
-        case 1:
-            self.fetchProductsWithIB(collection_id: "272069034031")
-        case 2:
-            self.fetchProductsWithIB(collection_id: "272069066799")
-        case 3:
-            self.fetchProductsWithIB(collection_id: "272069132335")
-            
-            
-        default:
-            break
-        }
-    }
-    
+  
     @IBAction func goToFavoriteMethod(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "showFavoriteFromCategorySegue", sender: Any?.self)
     }
@@ -61,36 +33,11 @@ class CategoryViewController: UIViewController {
     /*============================================*/
     override func viewDidLoad() {
         super.viewDidLoad()
-        let nib = UINib(nibName: "ProductsCollectionViewCell", bundle: nil)
-        self.productsClollectionView.register(nib, forCellWithReuseIdentifier: "productCell")
-        initVM()
 
         // Do any additional setup after loading the view.
     }
-    /*============================================*/
-    //MARK: Services Functions
-    func initVM() {
-        viewModel.reloadTableViewClosure = { [weak self] () in
-                   DispatchQueue.main.async {
-                       self?.productsClollectionView.reloadData()
-                   }
-               }
-               
-               viewModel.fetchProductAPI()
+    
 
-        
-    }
-    func  fetchProductsWithIB(collection_id : String){
-        viewModel.fetchProductAPIWithIB(collection_id: collection_id)
-        viewModel.reloadTableViewClosure = { [weak self] () in
-            DispatchQueue.main.async {
-                self?.productsClollectionView.reloadData()
-            }
-        }
-        
-    }
-    
-    
     /*
     // MARK: - Navigation
 
@@ -102,34 +49,3 @@ class CategoryViewController: UIViewController {
     */
 
 }
-extension CategoryViewController:UICollectionViewDelegate,UICollectionViewDataSource{
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-         return viewModel.numberOfCells
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "productCell", for: indexPath) as! ProductsCollectionViewCell
-        let cellVM = viewModel.getCellViewModel( at: indexPath )
-               cell.productListCellViewModel = cellVM
-               
-               return cell
-    }
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.viewModel.userPressed(at: indexPath)
-
-    }
-    
-    
-}
-//extension CategoryViewController{
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//           if let vc = segue.destination as? PhotoDetailViewController,
-//               let product = viewModel.selectedProduct {
-//               vc.imageUrl = product.image_url
-//           }
-//    }
-//}
-
